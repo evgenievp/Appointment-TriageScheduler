653c7f43a6e2467ff8f4bedaa27522d8c55b56f6
Victor Manin <victormanin151@gmail.com>
2026-08-24 00:38:15 +0300

    added findByEmail in DocService

--- backend/src/main/java/com/TriageScheduller/Triage/service/DoctorsService.java
+++ backend/src/main/java/com/TriageScheduller/Triage/service/DoctorsService.java
@@ -1,15 +1,19 @@
 package com.TriageScheduller.Triage.service;
 
 import com.TriageScheduller.Triage.models.Doctor;
+import com.TriageScheduller.Triage.models.User;
 import com.TriageScheduller.Triage.repo.DoctorsRepo;
+import com.TriageScheduller.Triage.repo.PatientsRepo;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DoctorsService {
     private final DoctorsRepo repo;
+    private final PatientsRepo patientsRepo;
 
-    public DoctorsService(DoctorsRepo repo) {
+    public DoctorsService(DoctorsRepo repo, PatientsRepo patientsRepo) {
         this.repo = repo;
+        this.patientsRepo = patientsRepo;
     }
 
     public Doctor findById(Long id) {
@@ -17,5 +21,12 @@ public class DoctorsService {
                 .orElseThrow(() -> new RuntimeException("Doctor not found"));
     }
 
+    public Doctor findByEmail(String email) {
 
+        User user = patientsRepo.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        return repo.findByUser(user)
+                .orElseThrow(() -> new RuntimeException("Doctor not found"));
+    }
 }
