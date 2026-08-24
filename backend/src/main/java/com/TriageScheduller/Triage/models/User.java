861f5bb0e496c2e1b1fe572279169ab4053b56b4
petar <evgenievp@yahoo.com>
2026-08-24 21:30:21 +0300

    fix: delete unnecessary constructor from User.java

--- backend/src/main/java/com/TriageScheduller/Triage/models/User.java
+++ backend/src/main/java/com/TriageScheduller/Triage/models/User.java
@@ -28,16 +28,6 @@ public class User {
     protected User() {
     }
 
-    public User(
-                String email,
-                String password,
-                String name,
-                String phone) {
-        this.email = email;
-        this.password = password;
-        this.name = name;
-        this.phone = phone;
-    }
 
     public User(String email,
                 String password,
