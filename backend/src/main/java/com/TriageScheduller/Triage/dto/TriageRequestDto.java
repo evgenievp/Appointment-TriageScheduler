92c297b966a355f0e62b11f7ed320892ec020135
Victor Manin <victormanin151@gmail.com>
2026-08-25 14:46:31 +0300

    made changes to TriageRequestDto and added validation

--- backend/src/main/java/com/TriageScheduller/Triage/dto/TriageRequestDto.java
+++ backend/src/main/java/com/TriageScheduller/Triage/dto/TriageRequestDto.java
@@ -1,9 +1,20 @@
 package com.TriageScheduller.Triage.dto;
 
-import com.TriageScheduller.Triage.utils.Priority;
+import com.TriageScheduller.Triage.utils.PainDuration;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 
 public record TriageRequestDto(
-        String answers,
-        int score,
-        Priority priority
+
+        @Min(1)
+        @Max(10)
+        int painLevel,
+
+        @NotNull
+        PainDuration painDuration,
+
+        boolean highTemperature,
+
+        boolean swelling
 ) {}
