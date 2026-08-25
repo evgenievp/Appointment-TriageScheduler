b3b234c4087326e2de7682c96ba8662ce82a7649
petar <evgenievp@yahoo.com>
2026-08-25 10:42:32 +0300

    feat: doctors repo functionality for random doctors

--- backend/src/main/java/com/TriageScheduller/Triage/repo/DoctorsRepo.java
+++ backend/src/main/java/com/TriageScheduller/Triage/repo/DoctorsRepo.java
@@ -3,9 +3,16 @@ package com.TriageScheduller.Triage.repo;
 import com.TriageScheduller.Triage.models.Doctor;
 import com.TriageScheduller.Triage.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface DoctorsRepo extends JpaRepository<Doctor, Long> {
     Optional<Doctor> findByUser(User user);
+
+
+    @Query(value = "select * from doctor order by random() limit 5", nativeQuery = true)
+    List<Doctor> findRandomDoctors();
+
 }
